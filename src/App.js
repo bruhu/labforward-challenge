4377b8d9f0b645258903d22478f98cf69e41e812
Bru <annabru.abh@gmail.com>
2019-09-16 17:18:28 +0200

    yellow world

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import BlueChart from "./components/BlueChart";
 import RedChart from "./components/RedChart";
+import YellowChart from "./components/YellowChart";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       {/* your code will go here */}
       <BlueChart />
       <RedChart />
+      <YellowChart />
     </div>
   );
 }
--- src/components/YellowChart.js
+++ src/components/YellowChart.js
@@ -2,25 +2,24 @@ import React, { Component } from "react";
 import "../../node_modules/react-vis/dist/style.css";
 import { XYPlot, LineSeries, XAxis, YAxis, VerticalBarSeries } from "react-vis";
 
-class RedChart extends Component {
+class YellowChart extends Component {
   render() {
     // prettier-ignore
     const dataImports = [
       {
-        title: "red chart",
-    data: [0,2,1,2,3,10,12,1,1,2,3,0,1,2,1,2,7,6,9,1,2,0,1,2,1,2,1,3,0,2,3,1,1,2,3,10,9,12,0,2,3,1,2,0,1,7,11,0,1,2],
-    signal:[0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,1,1,0,0,0]
-  }
+        title: "yellow chart",
+    data: [2,1,3,0,2,2,9,7,2,3,1,2,9,8,2,3,1,2,0,1,2,3,0,10,9,1,2,1,0,1,2,1,8,9,8,1,2,0,2,1,2,1,14,10,0,1,1,2,0,3],
+    signal:[0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0]}
     ];
 
     console.log(dataImports[0].data);
 
     return (
-      <div className="redchart-container">
+      <div className="yellowchart-container">
         <h2>Red Chart</h2>
         <div className="line-chart">
           <p>Data</p>
-          <XYPlot height={300} width={700} stroke="red">
+          <XYPlot height={300} width={700} stroke="yellow">
             <XAxis />
             <YAxis />
             <LineSeries
@@ -32,7 +31,7 @@ class RedChart extends Component {
         </div>
         <div className="bar-chart">
           <p>Signal</p>
-          <XYPlot height={300} width={700} color="red">
+          <XYPlot height={300} width={700} color="yellow">
             <XAxis />
             <YAxis />
             <VerticalBarSeries
@@ -47,4 +46,4 @@ class RedChart extends Component {
   }
 }
 
-export default RedChart;
+export default YellowChart;
