68308909768648c60702d21460fa8f4c0752da56
Bru <annabru.abh@gmail.com>
2019-09-16 16:42:50 +0200

    Created /components folder with BlueChart.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import BlueChart from "./components/BlueChart";
 
 function App() {
-  return <div className="App">{/* your code will go here */}</div>;
+  return (
+    <div className="App">
+      {/* your code will go here */}
+      <BlueChart></BlueChart>
+    </div>
+  );
 }
 
 export default App;
