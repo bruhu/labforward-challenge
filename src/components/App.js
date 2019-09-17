84e6f2611e1b7d86f6ab204666ff2d630370c773
Bru <annabru.abh@gmail.com>
2019-09-17 14:24:53 +0200

    Footer check.

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,10 @@ function App() {
       {dataSequences.map(dataSequence => (
         <Chart sequence={dataSequence} key={dataSequence.title} />
       ))}
+      <footer>
+        ( Charts are named after the colors they were represented in the original
+        sample data file for better differentiation and identification. )
+      </footer>
     </div>
   );
 }
