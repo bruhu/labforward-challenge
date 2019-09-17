300ddbfc82e49121ee8fa6dc89d9dfe9beb4eab4
Bru <annabru.abh@gmail.com>
2019-09-17 13:31:53 +0200

    Bracket hell

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -5,37 +5,37 @@ import { XYPlot, LineSeries, XAxis, YAxis, VerticalBarSeries } from "react-vis";
 class RedChart extends Component {
     render(){
         const{sequence} = this.props;
-    }
+    
     return (
-      <div className="redchart-container">
-        <h2>Red Chart</h2>
-        <div className="line-chart">
-          <p>Data</p>
-          <XYPlot height={300} width={700} stroke="#C0442E">
-            <XAxis />
-            <YAxis />
-            <LineSeries
-              data={dataImports[0].data.map((dataPoint, index) => {
-                return { x: index, y: dataPoint };
-              })}
-            />
-          </XYPlot>
+        <div className="redchart-container"/>
+          <h2>{sequence.title}</h2>
+          <div className="line-chart">
+            <p>Data</p>
+            <XYPlot height={300} width={700} stroke={sequence.style.strokeColor}>
+              <XAxis />
+              <YAxis />
+              <LineSeries
+                data={sequence.data.map((dataPoint, index) => {
+                  return { x: index, y: dataPoint };
+                })}
+              />
+            </XYPlot>
+          </div>
+          <div className="bar-chart">
+            <p>Signal</p>
+            <XYPlot height={300} width={700} color ={sequence.style.strokeColor}>
+              <XAxis />
+              <YAxis />
+              <VerticalBarSeries
+                data={sequence.signal.map((signalPoint, index) => {
+                  return { x: index, y: signalPoint };
+                })}
+              />
+            </XYPlot>
+          </div>
         </div>
-        <div className="bar-chart">
-          <p>Signal</p>
-          <XYPlot height={300} width={700} color="#C0442E">
-            <XAxis />
-            <YAxis />
-            <VerticalBarSeries
-              data={dataImports[0].signal.map((signalPoint, index) => {
-                return { x: index, y: signalPoint };
-              })}
-            />
-          </XYPlot>
-        </div>
-      </div>
-    );
+      );
+    }
   }
-}
 
 export default RedChart;
