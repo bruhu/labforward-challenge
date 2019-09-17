d327e78c34c1954fb280bd1751816e487670b2e3
Bru <annabru.abh@gmail.com>
2019-09-17 13:38:18 +0200

    Cleaned App.js - everything renders as wishhhhed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./App.css";
 import Chart from "./components/Chart";
-import BlueChart from "./components/BlueChart";
-import RedChart from "./components/RedChart";
-import YellowChart from "./components/YellowChart";
 import dataSequences from "./data/data.json";
 
 function App() {
@@ -12,9 +9,6 @@ function App() {
       {dataSequences.map(dataSequence => (
         <Chart sequence={dataSequence} key={dataSequence.title} />
       ))}
-      {/* <BlueChart />
-      <RedChart />
-      <YellowChart /> */}
     </div>
   );
 }
