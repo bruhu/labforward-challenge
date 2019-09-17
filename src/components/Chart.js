e080c7710321624fc17c23a07987a6eec9add68d
Bru <annabru.abh@gmail.com>
2019-09-17 13:36:01 +0200

    Added style.dataColor to JSON file for each chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,39 +3,38 @@ import "../../node_modules/react-vis/dist/style.css";
 import { XYPlot, LineSeries, XAxis, YAxis, VerticalBarSeries } from "react-vis";
 
 class Chart extends Component {
-    render(){
-        const{sequence} = this.props;
-    
+  render() {
+    const { sequence } = this.props;
     return (
-        <div className="chart-container"/>
-          <h2>{sequence.title}</h2>
-          <div className="line-chart">
-            <p>Data</p>
-            <XYPlot height={300} width={700} stroke={sequence.style.strokeColor}>
-              <XAxis />
-              <YAxis />
-              <LineSeries
-                data={sequence.data.map((dataPoint, index) => {
-                  return { x: index, y: dataPoint };
-                })}
-              />
-            </XYPlot>
-          </div>
-          <div className="bar-chart">
-            <p>Signal</p>
-            <XYPlot height={300} width={700} color ={sequence.style.strokeColor}>
-              <XAxis />
-              <YAxis />
-              <VerticalBarSeries
-                data={sequence.signal.map((signalPoint, index) => {
-                  return { x: index, y: signalPoint };
-                })}
-              />
-            </XYPlot>
-          </div>
+      <div className="chart-container">
+        <h2>{sequence.title}</h2>
+        <div className="line-chart">
+          <p>Data</p>
+          <XYPlot height={300} width={700} stroke={sequence.style.dataColor}>
+            <XAxis />
+            <YAxis />
+            <LineSeries
+              data={sequence.data.map((dataPoint, index) => {
+                return { x: index, y: dataPoint };
+              })}
+            />
+          </XYPlot>
         </div>
-      );
-    }
+        <div className="bar-chart">
+          <p>Signal</p>
+          <XYPlot height={300} width={700} color={sequence.style.dataColor}>
+            <XAxis />
+            <YAxis />
+            <VerticalBarSeries
+              data={sequence.signal.map((signalPoint, index) => {
+                return { x: index, y: signalPoint };
+              })}
+            />
+          </XYPlot>
+        </div>
+      </div>
+    );
   }
+}
 
 export default Chart;
