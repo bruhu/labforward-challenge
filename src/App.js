fa398c5acf43c7ce462174ebceb0e5c2a864ceeb
Bru <annabru.abh@gmail.com>
2019-09-17 13:42:07 +0200

    Created /styles folder, moved css files there

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./styles/App.css";
 import BlueChart from "./components/BlueChart";
 import RedChart from "./components/RedChart";
 import YellowChart from "./components/YellowChart";
